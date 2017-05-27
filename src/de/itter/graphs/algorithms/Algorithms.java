d02f9b41d2e9edaaec809d6adb8156566b3ba9ce
Erik Itter <Erik Itter>
2017-05-27 10:48:18 +0200

    API design: basics

---
+++ src/de/itter/graphs/algorithms/Algorithms.java
@@ -0,0 +1,35 @@
+package de.itter.graphs.algorithms;
+
+import java.util.Collection;
+
+import de.itter.graphs.api.Edge;
+import de.itter.graphs.api.Node;
+
+public class Algorithms {
+
+	
+	public static Collection<Edge> getInEdges(Node n){
+		return null;
+	}
+	
+	public static Collection<Edge> getOutEdges(Node n){
+		return null;
+	}
+	
+	public static Collection<Node> getPredecessors(Node n){
+		return null;
+		
+	}
+	
+	public static Collection<Node> getSuccessors(Node n){
+		return null;
+	}
+	
+	public static Collection<Node> getAncestors(Node n){
+		return null;
+	}
+	
+	public static Collection<Node> getProdgeny(Node n){
+		return null;
+	}
+}

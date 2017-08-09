aa0c3d8510ecb0cb42c1130518114d516f06ce88
Erik Itter <erik.itter@gmail.com>
2017-08-09 04:04:01 +0200

    OOD

--- src/de/itter/graphs/AbstractGraph.java
+++ src/de/itter/graphs/AbstractGraph.java
@@ -1,11 +1,22 @@
 package de.itter.graphs;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.itter.graphs.api.Graph;
+import de.itter.graphs.api.Node;
 
-public abstract class AbstractGraph implements Graph{
+public abstract class AbstractGraph implements Graph {
 
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private Collection<Node> nodes = new LinkedList<Node>();
+
+	public Collection<Node> getNodes() {
+		return nodes;
+	}
+
 }

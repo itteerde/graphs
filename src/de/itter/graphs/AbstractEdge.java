cd8a16216d2c4f9dd560c96b9423b9ddd94e0960
Erik Itter <erik.itter@gmail.com>
2017-08-09 03:52:16 +0200

    logging framework added

--- src/de/itter/graphs/AbstractEdge.java
+++ src/de/itter/graphs/AbstractEdge.java
@@ -1,5 +1,9 @@
 package de.itter.graphs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class AbstractEdge {
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 }
